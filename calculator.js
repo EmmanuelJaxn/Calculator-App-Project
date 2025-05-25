c9429410e8d6b4e31441dea4a785c6ab6defbe23
EmmanuelJaxn <angelmichaelz@gmail.com>
2025-05-25 04:05:19 -0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,18 @@
+//Calculator App JavaScript
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+  display.value += input;
+}
+
+
+
+function clearDisplay(){
+  display.value = "";
+}
+
+function calculate(){
+  display.value = eval(display.value);
+  
+}
